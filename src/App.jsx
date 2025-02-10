ce9627b32468900d9ffdd14e2173d7feb63d60e6
Praful-Daksh <prafuldaksh@proton.me>
2025-02-10 20:54:05 +0530

    app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import HomeCards from './components/HomeCards'
+
+function App() {
+  return (
+  <>
+  <Navbar />
+  <Hero/>
+  <HomeCards />
+  </>
+  )
+}
+
+export default App
