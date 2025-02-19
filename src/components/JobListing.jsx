aca4f3657b4200de88fcfe98801a39f6802edccb
Praful Daksh <prafuldaksh@proton.me>
2025-02-19 21:48:18 +0530

    Update JobListing.jsx

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useState } from 'react';
 
 const JobListing = (props) => {
-    const {job} = props;
+
+    const [showDescription, setShowDescription] = useState(false);
+    const { job } = props;
+    let description = job.description;
+
+    if (!showDescription) {
+        description = description.substring(0, 120) + '...';
+    }
     return (
         <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -9,13 +17,15 @@ const JobListing = (props) => {
                     <div className="text-gray-600 my-2">{job.type}</div>
                     <h3 className="text-xl font-bold">{job.company.name}</h3>
                 </div>
-                <div className="mb-5">{job.description}</div>
+                <div className="mb-5">{description}
+                    <button onClick={() => setShowDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-600">{showDescription ? 'See Less' : 'See More'}</button>
+                </div>
                 <h3 className="text-indigo-500 mb-2">{job.salary} Per Year</h3>
                 <div className="border border-gray-100 mb-5"></div>
                 <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                        <i className="fa-solid fa-location-dot text-lg"></i>
-                        {job.location}
+                            <i className="fa-solid fa-location-dot text-lg"></i>
+                            {job.location}
                     </div>
                     <a
                         href={``}
